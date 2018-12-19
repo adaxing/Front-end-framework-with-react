52151193443e83cf02da249acb2f9f7c07a893d6
adaxing <xing2@ualberta.ca>
2018-12-19 11:22:42 -0700

    Apply single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 import { DISHES } from './SharedComponent/Dish';
+import { COMMENTS } from './SharedComponent/Comments';
+import { LEADERS } from './SharedComponent/Leaders';
+import { PROMOTIONS } from './SharedComponent/Promotions';
+
 import Header from './SharedComponent/Header';
 import Home from './SharedComponent/Home';
 import Footer from './SharedComponent/Footer';
 
-
+import Contact from './ContactComponent/Contact';
 import Menu from './MenuComponent/Menu';
 //import  DishDetail  from './DishdetailComponent/Dishdetail';
 
@@ -15,25 +19,30 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dish : DISHES,
-            selected: null
+            dishes : DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
-        this.onDishSelect = this.onDishSelect.bind(this);
     }
-    onDishSelect(dishId){
-        this.setState({
-            selected: dishId
-        })
-    }
-    
+
     render() {
+        const HomePage = () => {
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo)=>promo.featured)[0]} 
+                    leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+                />
+            )
+        }
         return (
             <div>
                 <Header />
                 <Switch>
-                    <Route path="/home" component={Home} />
-                    <Route exact path="/menu" component={ ()=><Menu dishes={this.state.dish} /> } />
-                   
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={ ()=><Menu dishes={this.state.dishes} /> } />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
